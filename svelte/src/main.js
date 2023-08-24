f96afb5808284e810a2b87ecb1359220320a8f2a
Psykka <36780964+Psykka@users.noreply.github.com>
2023-08-23 22:13:42 -0300

    style(packages/ui): add ui package for all frameworks

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,3 +1,4 @@
+import '@frontend-comparison/ui'
 import App from './App.svelte'
 
 const app = new App({
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@frontend-comparison/ui'
 
 createApp(App).mount('#app')
