885813890a7aedf996d00b1bb28b8eb1f2e37642
Psykka <36780964+Psykka@users.noreply.github.com>
2023-08-24 17:27:27 -0300

    feat(svelte): create TODO component

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,1 +1,24 @@
-<h1>Svelte</h1>
+<script>
+    import Todo from "./components/Todo.svelte";
+
+    const todos = [
+        {
+            id: 1,
+            task: 'Fazer uma rotina de alongamento de 15 minutos',
+            completed: false
+        },
+        {
+            id: 2,
+            task: 'Programar JavaScript',
+            completed: false
+        }
+    ]
+</script>
+
+<h1 class="title">TODO Svelte</h1>
+
+<div class="content">
+    {#each todos as todo}
+        <Todo todo={todo} />
+    {/each}
+</div>
---
+++ svelte/src/components/Todo.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let todo;
+
+    const completeTodo = () => {
+        todo.completed = !todo.completed
+    }
+</script>
+
+<div class="card">
+    <input 
+        type="checkbox"
+        on:click={completeTodo}
+    >
+    <div class="card-content">
+        <p class={todo.completed ? "completed" : ""}>
+            {todo.task}
+        </p>
+    </div>
+    <button class="btn">
+        Delete
+    </button>
+</div>
